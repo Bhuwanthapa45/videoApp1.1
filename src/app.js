00692d8fa0a1b7fb6de26fbdd5ce5a6722e428f2
Bhuwan Thapa <hellobhuwanthapa45@gmail.com>
2024-11-04 12:51:20 +0545

    Adding Controllers

--- src/app.js
+++ src/app.js
@@ -2,15 +2,21 @@ import express from "express";
 import cors from "cors";
 import cookieParser from "cookie-parser";
 
-const app = express; 
+const app = express(); 
 app.use(cors({
      origin: process.env.CORS_ORIGIN,
-     credentials:true
-}))
+     credentials: true
+ }))
 app.use(express.json({limits: "16kb"}))
 app.use(express.urlencoded({extended: true, limit: "16kb"}))
 app.use(express.static("public"))
 app.use(cookieParser())
+
+//routes
+import router from "./routes/user.routes.js"
+
+//routedecleration
+app.use("/api/v1/users", router)
 
 export {app};
 
